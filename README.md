dc90f64cfb80c180f9b7ab301b91a5e7ca2e0ede
hygull <rishikesh0014051992@gmail.com>
2017-08-23 22:06:38 +0530

    4 created a node source for creating server(other way)

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Check a great wiki [here](https://github.com/hygull/node-runtime/wiki)
 
-Check [myserver.js](./examples/set1/myserver.js)
+Check [myserver.js](./examples/set1/myserver.js) - Creating a simple http server
+
+Check [myserver.js](./examples/set1/myserver_hostname.js) - How to pass hostname, port and a callback to **listen()**
---
+++ examples/set1/myserver_hostname.js
@@ -0,0 +1,39 @@
+/**
+	{
+		"date_of_creation": "23 August 2017",
+		"aim_of_source": 'Creating a simple http server',
+		"coded_by": 'Rishikesh Agrawani',
+	}
+*/
+
+//Including http module
+var http = require("http")
+
+//Setting port & ip of localhost
+var port = 8080
+var hostname = "127.0.0.1"
+
+//HTML text to display on the browser
+var htmlText = "<center>"+
+				"<h1 style='color:green'> Node.js is JavaScript Runtime</h1>"+
+				"<a href='https://nodejs.org/en/'>Visit here</a> to download"+
+				"</center>"
+
+//Printing simple log message
+console.log("Starting Node server")
+
+//Creting server
+server = http.createServer(
+			function(requset, response){
+				response.writeHead(200, {"Content-Type":"text/html"})
+				response.end(htmlText)
+			}
+		)
+
+//Our http server should listen on port 8080
+server.listen(port, hostname, function(){
+	console.log(`Server is running at http://${hostname}:${port}`)
+})
+
+//Printing log messages
+console.log("Server started which is listening on port 8080")
